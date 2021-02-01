1e2651d703a407c9f6d57ecb8b247b3f464b4ae0
Max Yuan <maxyuan6717@gmail.com>
2021-01-31 22:54:11 -0500

    change to user names

--- src/api/ship.js
+++ src/api/ship.js
@@ -5,7 +5,7 @@ import { Base } from "../util/base";
 // Create ship
 export const addShip = async (student1, student2) => {
   let newShip = axios.post(`${Base}/ship/add`, {
-    userIds: [
+    userNames: [
       student1.split(" ").slice(0, 2).join(" "),
       student2.split(" ").slice(0, 2).join(" "),
     ],
