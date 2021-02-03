13ed58feb4e29c97d8ba1c40ada0e46b79b108c5
Max Yuan <maxyuan6717@gmail.com>
2021-02-02 20:27:13 -0500

    thinner logo

--- src/pages/tempLanding.js
+++ src/pages/tempLanding.js
@@ -24,7 +24,7 @@ const tempLanding = () => {
     <StyledContainer>
       <div className="m-auto bob">
         <Row className="mx-auto mb-2 justify-content-center">
-          <img width={300} src={Logo} />
+          <img width={200} src={Logo} />
         </Row>
         <Row className="mx-auto justify-content-center">
           <StyledDescription>
